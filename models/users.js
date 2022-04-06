6ec357bc5718c161e8f8cf202fec44c6a42534df
PlomerFranco <franko.plomer@gmail.com>
2022-04-05 22:58:23 -0300

    first commit

---
+++ models/users.js
@@ -0,0 +1,19 @@
+
+const mongoose = require("mongoose")
+
+const usersCollection = 'users';
+
+const usersSchema = new mongoose.Schema({
+    user: {
+        type: String, 
+        require: true, 
+    },
+    pass: {
+        type: String, 
+        require: true, 
+    },
+})
+
+const users = mongoose.model(usersCollection, usersSchema);
+
+module.exports =  users
